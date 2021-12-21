053177c66a21010c9d5fbde03eac42a0d5b12781
Tobic0 <95741642+Tobic0@users.noreply.github.com>
2021-12-21 17:12:53 +0100

    Added DHT sensor

--- arduino_to_rasp.ino
+++ arduino_to_rasp.ino
@@ -1,5 +1,9 @@
+#include "DHT.h"
 //LED_BUILTIN to select the LED on the arduino UNO board
-#define PIR_PIN_INPUT 2 //PIR = Passive InfraRed sensor
+#define PIR_PIN_INPUT 3 //PIR = Passive InfraRed sensor
+#define DHTPIN 2     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11   // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
 
 bool alarm; //TRUE: Alarm is on, FALSE: Alarm is off
 bool alarmBreach; //TRUE: someone has breached the Alarm, FALSE: Alarm is on and everything is fine
@@ -9,11 +13,15 @@ void setup() {
   Serial.println("Arduino has been activated. Starting Serial communication...");
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(PIR_PIN_INPUT, INPUT);     
+  dht.begin();
   alarm = false;
   alarmBreach = false;
 }
 
 void loop() {
+  delay(500);
+  sendData();
+  
   if(alarm==true && digitalRead(PIR_PIN_INPUT) == HIGH && alarmBreach == false){
     Serial.println("MOTION_DETECTED");
     alarmBreach = true;
@@ -49,5 +57,22 @@ void loop() {
 }
 
 void sendData(){
-  Serial.println("");
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+  
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+  
+  Serial.println((String) "HUM"+h);
+  Serial.println((String) "TEM"+t);
+  Serial.println((String) "HIC"+hic);
 }
